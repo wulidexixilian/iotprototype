c0c2219dedb23d551b96c91499a53239c04f979f
wulidexixilian <31923805+wulidexixilian@users.noreply.github.com>
2017-09-14 10:31:14 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+    simple mqtt application.
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='simple_mqtt',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
